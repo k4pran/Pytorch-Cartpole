8272c958bd7de3b811e4d4f1a79c3a46a8f46b04
ryan <ryanmccauley211@gmail.com>
2019-12-01 10:53:37 +0000

    adjust hyper params

--- cartpole/hyper_params.py
+++ cartpole/hyper_params.py
@@ -3,11 +3,13 @@ from enum import Enum
 
 class HyperParams(Enum):
 
+    BATCH_SIZE = 32
     EPSILON = 0.9
     EPSILON_MIN = 0.01
     EPSILON_DECAY = 0.99
-    GAMMA = 0.9
+    GAMMA = 0.99
     LEARNING_RATE = 0.001
+    MEMORY_MAX = 1000
 
     @classmethod
     def has_value(cls, key):
